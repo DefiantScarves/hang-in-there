68e0ca471b8e6cd14c0d1bd02273cb062d1cc9d2
Caleb Young <calebyoung04@gmail.com>
2019-09-22 23:22:34 -0600

    Movement and camera

---
+++ Prototype/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float Speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVector = Vector3.zero;
+        movementVector.x = Input.GetAxis("Horizontal");
+        movementVector.z = Input.GetAxis("Vertical");
+
+        // Make movement happen in direction the camera is facing
+        movementVector = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movementVector;
+        GetComponent<Rigidbody>().AddForce(movementVector * Speed);
+
+        if (movementVector != Vector3.zero)
+        {
+            transform.forward = movementVector;
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform Player;
+    public Transform CameraTransform;
+
+    private Camera cam;
+
+    private float distanceFromPlayer = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensitivityX = 4.0f;
+    private float sensitivityY = 1.0f;
+
+    private const float Y_ANGLE_MIN = -50.0f;
+    private const float Y_ANGLE_MAX = 50.0f;
+
+    private void Start()
+    {
+        CameraTransform = transform; // Transform of object the script is on
+        cam = Camera.main;
+
+    }
+
+    private void Update()
+    {
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 direction = new Vector3(0, 0, -distanceFromPlayer);
+        Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
+        CameraTransform.position = Player.position + rotation * direction;
+        CameraTransform.LookAt(Player.position);
+    }
+}
