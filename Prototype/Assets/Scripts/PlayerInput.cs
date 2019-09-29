5a9e410e45125e66fa0da93158a6fa02354c229d
Caleb Young <calebyoung04@gmail.com>
2019-09-29 05:47:12 -0600

    Camera work, magnesis, and sprint

--- Prototype/Assets/Scripts/PlayerInput.cs
+++ Prototype/Assets/Scripts/PlayerInput.cs
@@ -1,51 +1,84 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
-    //delete this line
-    public float Speed = 10f;
+    public float Speed = 0.25f;
+    public float jumpForce = 7;
+    public float ScarfLength = 100f;
+
+    public LayerMask groundLayers;
+    public CapsuleCollider col;
+    public Image Crosshairs;
 
     private Rigidbody rb;
 
-    public LayerMask groundLayers;
-
-    public float jumpForce = 7;
-
-    public CapsuleCollider col;
+    private float currentSpeed;
+    private Vector3 heldObjectOffset;
+    private bool isAiming;
+    private bool readyToGrab;
+    private bool inMagnesis;
+    private Color standardMoveable;
+    private Color highlightedMoveable;
+    private Color selectedMoveable;
+    private GameObject inCrosshairs;
+    private GameObject heldObject;
+    private GameObject[] moveableObjects;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider>();
+        currentSpeed = Speed;
+        isAiming = false;
+        standardMoveable = new Color(0f, 0f, 0f);
+        highlightedMoveable = new Color(.93f, .15f, 1f);
+        selectedMoveable = new Color(1f, .89f, .255f);
+        moveableObjects = GameObject.FindGameObjectsWithTag("Moveable");
+        Crosshairs.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Get movement from WASD
         Vector3 movementVector = Vector3.zero;
         movementVector.x = Input.GetAxis("Horizontal");
         movementVector.z = Input.GetAxis("Vertical");
 
+        // Sprint
+        if (Input.GetKey(KeyCode.LeftShift)) { currentSpeed = Speed * 2; }
+        else { currentSpeed = Speed; }
+
+        // Aim 
+        Crosshairs.enabled = isAiming = Input.GetMouseButton(1);
+        if (isAiming)
+        {
+            if (!inMagnesis) { aim();}
+            if (readyToGrab && Input.GetMouseButton(0)) { Magnesis(); }
+        }
+        if (Input.GetMouseButtonUp(0)) { StopMagnesis(); }
+
+        // Zooms camera in to player
+        if (Input.GetMouseButtonDown(1)) { Camera.main.SendMessage("StartSkill"); }
+
+        // Zooms camera back out to orbit and clears any skill-specific stuff
+        if (Input.GetMouseButtonUp(1))
+        {
+            foreach (GameObject mgo in moveableObjects) { mgo.GetComponent<Renderer>().material.SetColor("_Color", standardMoveable); }
+            Camera.main.SendMessage("EndSkill");
+        }
+
         // Make movement happen in direction the camera is facing
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            Speed = Speed * 2;
-        }
-
-
         movementVector = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * movementVector;
-        GetComponent<Rigidbody>().AddForce(movementVector * Speed);
-
-        if (movementVector != Vector3.zero)
-        {
-            transform.forward = movementVector;
-        }
-
+        GetComponent<Rigidbody>().MovePosition(transform.position + movementVector * currentSpeed);
+        if (movementVector != Vector3.zero && !isAiming) { transform.forward = movementVector; }
         rb.AddForce(movementVector * Speed);
 
+        // Jump
         if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
@@ -56,5 +89,89 @@ public class PlayerInput : MonoBehaviour
     {
         return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
             col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
+    private void aim()
+    {
+        // Rotate character with camera
+        transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+
+        // Paint moveable objects that are visible
+        foreach (GameObject mgo in moveableObjects)
+        {
+            Ray toGo = new Ray(transform.position, mgo.transform.position - transform.position);
+            if (!inMagnesis && Physics.Raycast(toGo, out RaycastHit hitInfo)) {
+                if (hitInfo.collider.gameObject.tag == "Moveable")
+                {
+                    mgo.GetComponent<Renderer>().material.SetColor("_Color", highlightedMoveable);
+                }
+                else
+                {
+                    mgo.GetComponent<Renderer>().material.SetColor("_Color", standardMoveable);
+                }
+            }
+        }
+
+        // Paint moveable object being aimed at
+        if (!inMagnesis && Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit aimHit, ScarfLength))
+        {
+            if (aimHit.collider.gameObject.tag == "Moveable")
+            {
+                inCrosshairs = aimHit.collider.gameObject;
+                aimHit.collider.GetComponent<Renderer>().material.SetColor("_Color", selectedMoveable);
+                readyToGrab = true;
+            }
+            // If object aimed at is not moveable
+            else
+            {
+                readyToGrab = false;
+            }
+        }
+        // If no object is aimed at
+        else
+        {
+            readyToGrab = false;
+        }
+    }
+
+    private void Magnesis()
+    {
+        inMagnesis = true;
+        if (heldObject == null)
+        {
+            heldObject = inCrosshairs;
+            heldObjectOffset = (transform.position - Camera.main.transform.position) + (heldObject.transform.position - transform.position);
+        }
+        Ray fromCamera = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        float distanceFromCamera = Vector3.Distance(Camera.main.transform.position, Camera.main.transform.position + heldObjectOffset);
+        float distanceFromPlayer = Vector3.Distance(transform.position, Camera.main.transform.position + heldObjectOffset);
+        heldObject.GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(heldObject.transform.position, fromCamera.GetPoint(distanceFromCamera), 0.3f));
+        //heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, fromCamera.GetPoint(distanceFromCamera), 0.3f);
+
+        if (Input.GetKey(KeyCode.Q) && distanceFromPlayer > 5f)
+        {
+            //heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, transform.position, 0.025f);
+            heldObject.GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(heldObject.transform.position, transform.position, 0.025f));
+            heldObjectOffset = (transform.position - Camera.main.transform.position) + (heldObject.transform.position - transform.position);
+        }
+        if (Input.GetKey(KeyCode.E) && distanceFromPlayer < 25f)
+        {
+            //heldObject.transform.position = Vector3.Lerp(heldObject.transform.position, fromCamera.GetPoint(25f), 0.025f);
+            heldObject.GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(heldObject.transform.position, fromCamera.GetPoint(25f), 0.025f));
+            heldObjectOffset = (transform.position - Camera.main.transform.position) + (heldObject.transform.position - transform.position);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            StopMagnesis();
+        }
+    }
+
+    private void StopMagnesis()
+    {
+        readyToGrab = false;
+        inMagnesis = false;
+        heldObject = null;
+        inCrosshairs = null;
     }
 }
--- Prototype/Assets/Scripts/ThirdPersonCamera.cs
+++ Prototype/Assets/Scripts/ThirdPersonCamera.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ThirdPersonCamera : MonoBehaviour
@@ -10,6 +9,7 @@ public class ThirdPersonCamera : MonoBehaviour
     private Camera cam;
 
     private float distanceFromPlayer = 10.0f;
+    private float distanceFromPlayerInSkill = 5.0f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
     private float sensitivityX = 4.0f;
@@ -17,12 +17,22 @@ public class ThirdPersonCamera : MonoBehaviour
 
     private const float Y_ANGLE_MIN = -50.0f;
     private const float Y_ANGLE_MAX = 50.0f;
+    private bool playerInSkill;
+    private bool inAimingPosition;
+    private bool inFollowingPosition;
+    private Vector3 aimingOffset;
+    private Vector3 aimingPosition;
+    private Vector3 followingPosition;
 
     private void Start()
     {
         CameraTransform = transform; // Transform of object the script is on
         cam = Camera.main;
-
+        playerInSkill = false;
+        aimingOffset = transform.position - Player.transform.position;
+        aimingPosition = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * aimingOffset;
+        inAimingPosition = false;
+        inFollowingPosition = true;
     }
 
     private void Update()
@@ -33,11 +43,75 @@ public class ThirdPersonCamera : MonoBehaviour
         currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
     }
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         Vector3 direction = new Vector3(0, 0, -distanceFromPlayer);
         Quaternion rotation = Quaternion.Euler(-currentY, currentX, 0);
-        CameraTransform.position = Player.position + rotation * direction;
-        CameraTransform.LookAt(Player.position);
+
+        // Orbit and zoomed positions
+        followingPosition = Player.position + rotation * direction;
+        aimingPosition = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * aimingOffset;
+
+        if (!playerInSkill)
+        {
+            inAimingPosition = false;
+            Invoke("StopZoomOut", .5f);
+
+            // This block is camera behavior while zooming back out to orbit
+            if (!inFollowingPosition)
+            {
+                CameraTransform.position = Vector3.Lerp(CameraTransform.position, followingPosition, .1f);
+                Quaternion newRotation = Quaternion.LookRotation(Player.position - CameraTransform.position);
+                CameraTransform.rotation = Quaternion.RotateTowards(CameraTransform.rotation, newRotation, .9f);
+            }
+            // This block is camera orbit behavior
+            else
+            {
+                CameraTransform.position = followingPosition;
+                CameraTransform.LookAt(Player.transform);
+            }
+        }
+        else
+        {
+            inFollowingPosition = false;
+            Invoke("StopZoomIn", .3f);
+
+            // This block is camera behavior while zooming in to aiming
+            if (!inAimingPosition)
+            {
+                CameraTransform.rotation = Quaternion.Euler(-currentY, currentX, 0);
+                CameraTransform.position = Vector3.Lerp(CameraTransform.position, Player.position + aimingPosition, .2f);
+            }
+            // This block is camera behavior while aiming
+            else
+            {
+                CameraTransform.rotation = Quaternion.Lerp(CameraTransform.rotation, Quaternion.Euler(-currentY, currentX, 0), 0.5f);
+                CameraTransform.position = Vector3.Lerp(CameraTransform.position, Player.position + aimingPosition, .2f);
+            }
+        }
+    }
+
+    // StartSkill tells the camera to zoom in
+    public void StartSkill()
+    {
+        playerInSkill = true;
+        aimingPosition = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * aimingOffset;
+    }
+
+    // EndSkill tells the camera to zoom out
+    public void EndSkill()
+    {
+        playerInSkill = false;
+        aimingPosition = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * aimingOffset;
+    }
+
+    private void StopZoomIn()
+    {
+        inAimingPosition = true;
+    }
+
+    private void StopZoomOut()
+    {
+        inFollowingPosition = true;
     }
 }
