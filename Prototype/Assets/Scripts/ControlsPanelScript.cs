fd687945d31b5bb0af59e5f811e058922d0812f6
Jonathan Ryan Wigderson <jrwigd@gmail.com>
2019-12-01 12:58:03 -0700

    PauseMenu re-added

---
+++ Prototype/Assets/Scripts/ControlsPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsPanelScript : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject PanelCommands;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+
+        if (PanelCommands != null)
+        {
+            bool isActive = PanelCommands.activeSelf;
+            PanelCommands.SetActive(!isActive);
+        }
+    }
+}
